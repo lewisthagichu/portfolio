6a685d845e6d735839d1d916d304e94389b83066
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-21 18:32:44 +0300

    Add nextCase component

--- components/Case/Case.jsx
+++ components/Case/Case.jsx
@@ -2,21 +2,26 @@ import Footer from '../Footer/Footer';
 import CaseBody from './CaseBody/CaseBody';
 import styles from './case.module.scss';
 import { ArgesHeavy } from '@/public/fonts/fonts';
-import Image from 'next/image';
+import { useRef } from 'react';
+import { useScroll, useTransform, motion } from 'framer-motion';
 
 function Case({ project }) {
-  const { title, src } = project;
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <h2 className={ArgesHeavy.className}>{title}</h2>
-      </div>
+  const { title } = project;
+  const container = useRef(null);
 
-      <div className={styles.imageContainer}>
-        <div className={styles.imageElement}>
-          <Image src={`${src}/web1.jpg`} priority fill alt="image" />
-        </div>
-      </div>
+  const { scrollYProgress: titleProgress } = useScroll({
+    target: container,
+    offset: ['start start', '800px start'],
+  });
+
+  const opacity = useTransform(titleProgress, [0, 1], [1, 0]);
+  const scale = useTransform(titleProgress, [0, 1], [1, 0.3]);
+  const y = useTransform(titleProgress, [0, 0.9], [0, 70]);
+  return (
+    <div ref={container} className={styles.container}>
+      <motion.div style={{ opacity, scale, y }} className={styles.title}>
+        <h2 className={ArgesHeavy.className}>{title}</h2>
+      </motion.div>
 
       <CaseBody project={project} />
       <Footer />
---
+++ components/Case/NextCase/NextCase.jsx
@@ -0,0 +1,7 @@
+import styles from './nextCase.module.scss';
+
+function NextCase() {
+  return <div>NextCase</div>;
+}
+
+export default NextCase;
