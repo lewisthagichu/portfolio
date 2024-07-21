67b3b08bc4ec4fa2742f3cf0e60e7fcef05dbcca
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-21 16:26:09 +0300

    Add curved border to dynamic work routes

--- components/Case/CaseBody/CaseBody.jsx
+++ components/Case/CaseBody/CaseBody.jsx
@@ -2,13 +2,24 @@ import CurvedBorder from '@/components/common/CurvedBorder';
 import Bio from '../Bio/Bio';
 import MediaContent from '../MediaContent/MediaContent';
 import styles from './caseBody.module.scss';
+import { useRef } from 'react';
+import { useScroll, useTransform } from 'framer-motion';
 
 function CaseBody({ project }) {
   const { src } = project;
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
+
+  const height = useTransform(scrollYProgress, [0, 1], [200, 0]);
   return (
-    <section className={styles.container}>
+    <section ref={container} className={styles.container}>
       <Bio project={project} />
       <MediaContent src={src} />
+      <CurvedBorder height={height} />
     </section>
   );
 }
