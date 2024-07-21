079e40f43049dd31759ddcdf7bfccb76938fd828
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-21 16:40:42 +0300

    Add scrolling parallax to LargeImage

--- components/Case/MediaContent/LargeImage.jsx
+++ components/Case/MediaContent/LargeImage.jsx
@@ -1,10 +1,21 @@
 import styles from './mediaContent.module.scss';
 import Image from 'next/image';
+import { useRef } from 'react';
+import { useScroll, useTransform, motion } from 'framer-motion';
 
 function LargeImage({ src }) {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 0.5, 1], [-200, 0, 200]);
+
   return (
-    <div className={styles.largeImage}>
-      <div className={styles.imageContainer}>
+    <div ref={container} className={styles.largeImage}>
+      <motion.div style={{ y }} className={styles.imageContainer}>
         <Image
           width={0}
           height={0}
@@ -12,7 +23,7 @@ function LargeImage({ src }) {
           src={`${src}/web2.jpg`}
           alt="Project Image"
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
