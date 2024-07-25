23e69befd5cf78041419ab8a1d6efa0cc5247e3c
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-25 12:32:21 +0300

    Declutter Project component

--- components/Work/Project/Project.jsx
+++ components/Work/Project/Project.jsx
@@ -1,50 +1,18 @@
 import styles from './project.module.scss';
-import { useRef, useState } from 'react';
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useState } from 'react';
 import { NeuehaasBody } from '@/public/fonts/fonts';
-import Image from 'next/image';
 import Link from 'next/link';
-import ProjectTitle from './ProjectTitle';
 import Cursor from '@/components/common/Cursor';
-import VideoComponent from '@/components/common/VideoComponent';
+import ProjectTitle from './ProjectTitle';
+import ProjectMedia from './ProjectMedia';
 
 function Project({ title, description, link }) {
   const [active, setActive] = useState(false);
 
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ['start end', 'end start'],
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
-
   return (
     <section className={`${styles.container} ${NeuehaasBody.className}`}>
       <Link href={link}>
-        <div
-          ref={container}
-          onMouseEnter={() => setActive(true)}
-          onMouseLeave={() => setActive(false)}
-          className={styles.imageContainer}
-        >
-          <motion.div style={{ y }} className={styles.image}>
-            <Image
-              src={`/media${link}/web1.jpg`}
-              width={0}
-              height={0}
-              sizes="100vw"
-              alt="image"
-            />
-          </motion.div>
-
-          <VideoComponent
-            videoContainer={styles.videoContainer}
-            video={styles.video}
-            videoElement={styles.videoElement}
-            link={link}
-          />
-        </div>
+        <ProjectMedia link={link} setActive={setActive} />
       </Link>
 
       <ProjectTitle title={title} description={description} />
---
+++ components/Work/Project/ProjectMedia.jsx
@@ -0,0 +1,43 @@
+import styles from './project.module.scss';
+import { useRef } from 'react';
+import { useScroll, useTransform, motion } from 'framer-motion';
+import Image from 'next/image';
+import VideoComponent from '@/components/common/VideoComponent';
+
+function ProjectMedia({ setActive, link }) {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
+
+  return (
+    <div
+      ref={container}
+      onMouseEnter={() => setActive(true)}
+      onMouseLeave={() => setActive(false)}
+      className={styles.imageContainer}
+    >
+      <motion.div style={{ y }} className={styles.image}>
+        <Image
+          src={`/media${link}/web1.jpg`}
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="image"
+        />
+      </motion.div>
+
+      <VideoComponent
+        videoContainer={styles.videoContainer}
+        video={styles.video}
+        videoElement={styles.videoElement}
+        link={link}
+      />
+    </div>
+  );
+}
+
+export default ProjectMedia;
