3fb31c5437d0528415e0e7c707579865047cbf74
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-27 12:16:30 +0300

    Add head metadata

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,14 +2,40 @@ import '@/styles/normalize.css';
 import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 import Header from '@/components/Header/Header';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Lewis Thagichu Full-stack Developer</title>
+        <meta
+          name="description"
+          content="Lewis Thagichu is a software engineer based in Nairobi, Kenya. Keen on partnering with ambitious brands to develop innovative
+          software."
+        />
+        <meta name="author" content="Lewis Thagichu" />
+        <meta
+          name="keywords"
+          content="portfolio, software engineer, web developer, Nairobi, Kenya"
+        />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:site" content="@thagichucodes" />
+        <meta name="twitter:title" content="Lewis Thagichu's Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Lewis Thagichu is a software engineer based in Nairobi, Kenya."
+        />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header />
       <AnimatePresence mode="wait">
         <Component key={router.asPath} {...pageProps} />
       </AnimatePresence>
-    </div>
+    </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,13 @@
-import Head from 'next/head';
 import Home from '@/components/Home/Home';
 import PageTransition from '@/components/PageTransition/PageTransition';
 import SmoothScroll from '@/components/common/SmoothScroll';
 
 export default function HomePage() {
   return (
-    <>
-      <Head>
-        <title>Lewis Thagichu Full-stack Developer</title>
-        <meta name="description" content="Developed by Lewis Thagichu" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <PageTransition>
-        <SmoothScroll>
-          <Home />
-        </SmoothScroll>
-      </PageTransition>
-    </>
+    <PageTransition>
+      <SmoothScroll>
+        <Home />
+      </SmoothScroll>
+    </PageTransition>
   );
 }
