49858e028bd2b19f9092a447be661f470b890995
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-29 07:16:18 +0300

    Reveal nav text after it has fully opened

--- utils/getChars.js
+++ utils/getChars.js
@@ -6,7 +6,7 @@ export const getChars = (word) => {
   word.split('').forEach((char, i) => {
     chars.push(
       <motion.span
-        custom={[i === 0 ? 0.38 : 0.38 + i * 0.01, (word.length - i) * 0.01]}
+        custom={[i === 0 ? 0.3 : 0.3 + i * 0.01, (word.length - i) * 0.01]}
         variants={translate}
         initial="initial"
         animate="enter"
