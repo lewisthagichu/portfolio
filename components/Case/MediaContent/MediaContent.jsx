24e5ca7755a90b142c370bde298ca5bd38dbdd8e
lewisthagichu <lewisthagichu254@gmail.com>
2024-07-31 15:58:49 +0300

    Fix padding in case's video container

--- components/Case/MediaContent/MediaContent.jsx
+++ components/Case/MediaContent/MediaContent.jsx
@@ -5,20 +5,22 @@ import MobileImages from './MobileImages';
 function MediaContent({ src }) {
   return (
     <div className={styles.container}>
-      <div className={styles.videoContainer}>
-        <video
-          className={styles.videoElement}
-          autoPlay
-          muted
-          playsInline
-          loop
-          aria-label="Video player"
-          preload="none"
-        >
-          <source src={`${src}/workVideo.mp4`} type="video/mp4" />
-          <source src={`${src}/workVideo.webm`} type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+      <div className={styles.pad}>
+        <div className={styles.videoContainer}>
+          <video
+            className={styles.videoElement}
+            autoPlay
+            muted
+            playsInline
+            loop
+            aria-label="Video player"
+            preload="none"
+          >
+            <source src={`${src}/workVideo.mp4`} type="video/mp4" />
+            <source src={`${src}/workVideo.webm`} type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
 
       <LargeImage src={src} />
