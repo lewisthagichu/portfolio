ec35d370bde901e9cb13061eaec364277bac6873
lewisthagichu <lewisthagichu254@gmail.com>
2024-09-02 19:51:33 +0300

    refactor: Remove opacity animation

--- components/Home/Description/Description.jsx
+++ components/Home/Description/Description.jsx
@@ -1,76 +1,20 @@
 import styles from './description.module.scss';
-import { useRef } from 'react';
-import { motion, useScroll } from 'framer-motion';
 import { expertises, paragraphs } from '@/data/aboutData';
 import { NeuehaasBody } from '@/public/fonts/fonts';
-import useSmoothOpacity from '@/hooks/useSmoothOpacity';
 import MagneticButton from '@/components/common/MagneticButton';
 
-const Word = ({ children, progress, range }) => {
-  const smoothOpacity = useSmoothOpacity(progress, range, [0, 1]);
-  return (
-    <span className={styles.word}>
-      <span className={styles.shadow}>{children}</span>
-      <motion.span style={{ opacity: smoothOpacity }}>{children}</motion.span>
-    </span>
-  );
-};
-
-const Paragraph = ({ paragraph, progress }) => {
-  const words = paragraph.split(' ');
-
-  return (
-    <p className={styles.paragraph}>
-      {words.map((word, index) => {
-        const start = index / words.length;
-        const end = start + 1 / words.length;
-        return (
-          <Word key={index} progress={progress} range={[start, end]}>
-            {word}
-          </Word>
-        );
-      })}
-    </p>
-  );
-};
-
 function Description() {
-  const containerRef = useRef(null);
-
-  const { scrollYProgress: scrollYProgress1 } = useScroll({
-    target: containerRef,
-    offset: ['start 0.98', 'end 0.92'],
-  });
-
-  const { scrollYProgress: scrollYProgress2 } = useScroll({
-    target: containerRef,
-    offset: ['start 0.48', 'end 0.4'],
-  });
-
-  const divOpacity = useSmoothOpacity(
-    scrollYProgress2,
-    [0, 0.9, 1],
-    [0, 0.2, 1]
-  );
-
   return (
     <section className={`${styles.container} ${NeuehaasBody.className}`}>
       <div className={styles.wrapper}>
-        <div ref={containerRef} className={styles.description}>
-          {paragraphs.map((paragraph, paragraphIndex) => (
-            <Paragraph
-              key={paragraphIndex}
-              paragraph={paragraph}
-              progress={
-                paragraphIndex === 0 ? scrollYProgress1 : scrollYProgress2
-              }
-            />
+        <div className={styles.description}>
+          {paragraphs.map((paragraph, index) => (
+            <p key={index} className={styles.paragraph}>
+              {paragraph}
+            </p>
           ))}
         </div>
-        <motion.div
-          style={{ opacity: divOpacity }}
-          className={styles.expertise}
-        >
+        <div className={styles.expertise}>
           <h4>SKILLS</h4>
           <div>
             {expertises.map((expertise, index) => (
@@ -79,7 +23,7 @@ function Description() {
               </p>
             ))}
           </div>
-        </motion.div>
+        </div>
       </div>
 
       <MagneticButton
