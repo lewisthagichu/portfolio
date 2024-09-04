b613e63ef40f7b52ad5c73a3f4a319ac07d087cd
lewisthagichu <lewisthagichu254@gmail.com>
2024-09-04 15:20:36 +0300

    refactor: Conditionally render home or work video

--- components/Case/MediaContent/MediaContent.jsx
+++ components/Case/MediaContent/MediaContent.jsx
@@ -24,7 +24,14 @@ function MediaContent({ src, slogan, title }) {
               }`}
               type="video/webm"
             />
-            {/* <source src={`${src}/workVideo.mp4`} type="video/mp4" /> */}
+            <source
+              src={`${src}/${
+                src === 'media/work/tictacplay' || src === 'media/work/ignite'
+                  ? 'workVideo.mp4'
+                  : 'homeVideo.mp4'
+              }`}
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
         </div>
